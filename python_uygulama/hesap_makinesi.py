d7930c9d1df67c993954821039975111b0a9cda6
Mert Karaosmanoğlu <mertkaraosmanoglu745@gmail.com>
2025-01-26 15:45:38 +0300

    Tanımlanan fonksiyonlar kullanılarak yeniden düzenlendi. 0'a bölme işlemi kontrolu fonksiyonun içine taşındı. Geçersiz seçim yapabilme hatası düzeltildi.

--- python_uygulama/hesap_makinesi.py
+++ python_uygulama/hesap_makinesi.py
@@ -9,9 +9,11 @@ def carp(a, b):
     return a * b
 
 def bol(a, b):
-    return a / b
+    if b == 0:
+        return "0'a bolme islemi yapilamaz!"
+    else:
+        return a / b
     
-
 while True:
     print("1. Toplama\n2. Cikarma\n3. Carpma\n4. Bolme\n5. Cikis")
     secim = input("Secim yapiniz: ")
@@ -19,20 +21,19 @@ while True:
     if secim == '5':
         break
 
+    if secim not in ["1","2","3","4"]:
+        print("Gecersiz secim")
+    
     sayi1 = float(input("Birinci sayiyi girin: "))
     sayi2 = float(input("Ikinci sayiyi girin: "))
 
     if secim == '1':
-        print("Sonuc:", sayi1 + sayi2)
+        sonuc = topla(sayi1, sayi2)
     elif secim == '2':
-        print("Sonuc:", sayi1 - sayi2)
+        sonuc = cikar(sayi1, sayi2)
     elif secim == '3':
-        print("Sonuc:", sayi1 * sayi2)
+        sonuc = carp(sayi1, sayi2)
     elif secim == '4':
-        if sayi2 != 0:
-            print("Sonuc:", sayi1 / sayi2)
-        else:
-            print("0'a bolme islemi yapilamaz!")
-    else:
-        print("Gecersiz secim.")
+        sonuc = bol(sayi1, sayi2)
 
+    print("Sonuç:", sonuc)
